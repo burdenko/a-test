749df80cc19b37e2d2ad6ebfd2471714d617fcf6
Logvinenko Oleg <logvin.ol@mail.ru>
2016-09-09 04:32:48 +0300

    09.09.2016_1

--- app/agent-list/agent-list.component.js
+++ app/agent-list/agent-list.component.js
@@ -1,75 +1,64 @@
 'use strict';
 
- 
 angular.
  module('agentList' ).
   component('agentList',{
    templateUrl:'agent-list/agent-list.template.html', 
     controller: ['$http', function ($http){
 
+    var self = this;
+    $http.get('agent-list/agent-list.json').then(function(resp, status, headers, config) {
+      console.log('This is data:', resp, '\n\n This is status:',status, '\n\n This is Headers:', headers, '\n\n This is Confog', config);
+      self.agents = resp.data;
+    });
 
- var self = this;
-      $http.get('agent-list/agent-list.json').then(function(resp, status, headers, config) {
-      	 console.log('This is data:', resp, '\n\n This is status:',status, '\n\n This is Headers:', headers, '\n\n This is Confog', config);
-        self.agents = resp.data;
+    this.sortFlag=undefined;
+    
+    this.sort= function (fieldSort) {
+      this.sortFlag= this.sortFlag==fieldSort ? '-'+fieldSort : fieldSort;
+    };
+
+    this.upFlag= function (fieldSort) {
+      if (this.sortFlag==fieldSort) return true;
+      if (this.sortFlag=='-'+fieldSort) return false;
+    };
+
+    this.deleteAgent=function(id) {
+      var yes=false;
+      yes=confirm('Вы действительно хотите удалить эту запись?');
+      if (yes==true)
+        for (var i = 0; i < this.agents.length; i++) {
+          if (this.agents[i].idagent==id) this.agents.splice(i, 1);
+        }
+    };
+
+    this.addNewAgent=function() {
+      var maxElement=0;
+      
+      for (var i = 0; i < this.agents.length; i++) {
+        if (this.agents[i].idagent>maxElement) maxElement=this.agents[i].idagent;
+      }
+
+      this.agents.push(
+      {
+        idagent: +maxElement+1,
+        firstname: 'New', 
+        lastname: '',
+        email: '',
+        phone:'',
+        birthday:'',
+        edit:false
       });
+    };
 
- 
+    this.hide=false;
+    this.editAgent=function(id)	{
+      this.hide=!this.hide;
+      for (var i = 0; i < this.agents.length; i++) {
+        if (this.agents[i].idagent==id) this.agents[i].edit=!this.agents[i].edit;
+      }
+    }
 
-
-
-
-//	this.sortFlag="firstname";
-	this.sortFlag=undefined;
-	this.sort= function (poleSort)
-		{
-		this.sortFlag= this.sortFlag==poleSort ? '-'+poleSort : poleSort;
-		};
-
-	this.upFlag= function (poleSort)
-		{
-		if (this.sortFlag==poleSort) return true;
-		if (this.sortFlag=='-'+poleSort) return false;
-		};
-
-	this.deleteAgent=function(id)
-		{
-			var yes=false;
-			yes=confirm('Вы действительно хотите удалить эту запись?');
-			if (yes==true)
-				for (var i = 0; i < this.agents.length; i++) {
- 					if (this.agents[i].idagent==id) this.agents.splice(i, 1);
-					}
-		};
-
-	this.addNewAgent=function()
-		{
-			var maxElement=0;
-			for (var i = 0; i < this.agents.length; i++) {
- 				if (this.agents[i].idagent>maxElement) maxElement=this.agents[i].idagent;
-			
-				}
-
-			this.agents.push({
-			idagent: +maxElement+1,
-			firstname: 'none', 
-			lastname: 'none',
-			email: 'none',
-			phone:'none',
-			birthday:'none'
-  			});
-		this.proverka=this.agents.length;
-		};
-	
-	 this.hide=false;
-	 this.redactAgent=function(id)
-		{
-			this.hide=!this.hide;
-			for (var i = 0; i < this.agents.length; i++) {
- 				if (this.agents[i].idagent==id) this.agents[i].redact=!this.agents[i].redact;
-			}
-		}
-			
-} ]
-});
+    }]
+  });
 
