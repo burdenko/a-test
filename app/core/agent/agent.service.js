071d0edc7535aaab1adf5cafc9d0591f3375fa7d
Logvinenko Oleg <logvin.ol@mail.ru>
2016-09-13 13:56:19 +0300

    End2

--- app/core/agent/agent.service.js
+++ app/core/agent/agent.service.js
@@ -4,47 +4,47 @@ angular.
   module('core.agent').
   factory('Agent', ['$resource',
     function($resource) {
-      return $resource(':agentId.json', {}, {
+      return $resource('json/:agentId.json', {}, {
         query1: {
           method: 'GET',
-          params: {agentId: 'agent-list/agent-list1'},
+          params: {agentId: 'agent-list1'},
           isArray: true
         },
         query2: {
           method: 'GET',
-          params: {agentId: 'agent-list/agent-list2'},
+          params: {agentId: 'agent-list2'},
           isArray: true
         },
         query3: {
           method: 'GET',
-          params: {agentId: 'agent-list/agent-list3'},
+          params: {agentId: 'agent-list3'},
           isArray: true
         },
         query4: {
           method: 'GET',
-          params: {agentId: 'agent-list/agent-list4'},
+          params: {agentId: 'agent-list4'},
           isArray: true
         },
         query5: {
           method: 'GET',
-          params: {agentId: 'operation-type/operation-type1'},
+          params: {agentId: 'operation-type1'},
           isArray: true
         },
         query6: {
           method: 'GET',
-          params: {agentId: 'operation-type/operation-type2'},
+          params: {agentId: 'operation-type2'},
           isArray: true
         }
         ,
         query7: {
           method: 'GET',
-          params: {agentId: 'transaction-journal/transaction-journal1'},
+          params: {agentId: 'transaction-journal1'},
           isArray: true
         }
         ,
         query8: {
           method: 'GET',
-          params: {agentId: 'transaction-journal/transaction-journal2'},
+          params: {agentId: 'transaction-journal2'},
           isArray: true
         }
       });
